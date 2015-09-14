6f3d7c918fb2d91139d86cc9f98330c0ca7335a8
Max Kindred <maxwell.kindred@drake.edu>
2015-09-13 22:42:46 -0500

    added code for my section, it has a picture of shia and a button that says just do it and when you click it is supposed to switch to a gif of him saying just do it, but i dont know how to get the gif to work.

--- app/src/main/java/com/assignment3/jesse/assign3/Max.java
+++ app/src/main/java/com/assignment3/jesse/assign3/Max.java
@@ -4,13 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Max extends AppCompatActivity {
 
+    Button button;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_max);
+
+        addListenerOnButton();
+
+
+    }
+
+    public void addListenerOnButton(){
+        image = (ImageView) findViewById(R.id.imageView1);
+
+        button = (Button) findViewById(R.id.maxButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              image.setImageResource(R.drawable.justdoitlaboof);
+            }
+        });
     }
 
     @Override
